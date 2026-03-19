6eabb6cc5d05475e1bdb0ff589350728e3d03b80
kevinferns25-dotcom <kevinferns25@gmail.com>
2026-03-19 11:07:49 +0530

    Reseveration
    Co-Authored-By: bhargav <194670496+Bhargav029@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Menu from './pages/Menu';
 import Checkout from './pages/Checkout';
 import Login from './pages/Login'; // Import the Login page
 import Settings from './pages/Settings';
+import Reservation from "./pages/Reservation";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Route path="/menu" element={<Menu />} />
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/login" element={<Login />} /> {/* Add this route */}
-      <Route path="/settings" element={<Settings />} /> {/* Add this route */}
+      <Route path="/settings" element={<Settings />} /> 
+      <Route path="/reservations" element={<Reservation />} />
     </Routes>
   );
 }
