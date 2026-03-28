d5894d3687e53ac837e37d2f9b50ecdef3b5e8d0
bhargav <bhargavnageshri029@gmail.com>
2026-03-28 16:05:55 +0530

    deployed

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -8,13 +8,6 @@ import NavbarMain from "../components/NavbarMain";
 import { useCart } from '../context/CartContext'; 
 import { useAuth } from '../context/AuthContext'; 
 
-import breakfast from "/assets/breakfast.jpg";
-import starters from "/assets/starters.jpg";
-import burger from "/assets/burger.jpg";
-import eggs from "/assets/eggs.jpg";
-import pasta from "/assets/pasta.jpg";
-import eggs from "/assets/eggs.jpg";
-import indianVeg from "/assets/indianVeg.jpg";
 
 const menuData = [
   // ... (Your menuData remains identical)
